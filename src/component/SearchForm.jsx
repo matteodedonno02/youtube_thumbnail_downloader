2191ac2a2f00048938f761b4db184397405a5077
Matteo De Donno <matteodedonno02@gmail.com>
2022-07-21 19:35:21 +0200

    first version working

--- src/component/SearchForm.jsx
+++ src/component/SearchForm.jsx
@@ -1,9 +1,20 @@
+import { useRef } from "react"
+import "./SearchForm"
+
 export default function SearchForm() {
+
+  const url = useRef("")
+  const form = useRef()
+
+  const customSubmit = (e) => {
+    form.current.action = "https://i.ytimg.com/vi_webp/" + url.current.value.split("watch?v=")[1] + "/maxresdefault.webp"
+  }
+
   return (
-    <form className="search-form">
+    <form ref={form} className="search-form" method="GET" action="" onSubmit={(e) => { customSubmit(e) }}>
       <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">Youtube Video's URL</label>
-        <input type="text" className="form-control" />
+        <label className="form-label">Youtube Video's URL</label>
+        <input ref={url} type="text" className="form-control" />
       </div>
       <button type="submit" className="btn btn-primary">Download</button>
     </form>
